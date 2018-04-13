6511422fd3afc523fb7b093d1e5c3895e0a87aee
DESKTOP-OG1U6QC\rmill <rmill013@gmail.com>
2018-04-12 22:21:15 -0400

    Initial Commit

---
+++ Assets/Scripts/CameraLerpToTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerpToTransform : MonoBehaviour {
+
+	public Transform			camTarget;
+	public float				trackingSpeed;
+	public float				minX;
+	public float				minY;
+	public float 				maxX;
+	public float				maxY;
+
+	// We will be using FixedUpdate to track the player because our player will move based
+	// on Physics. Therefore we want the camera to update in sync with the Physics Engine thus using FixedUpdate and not Update()
+	void FixedUpdate () {
+
+		if (camTarget != null)
+		{
+			Vector2 newPos = Vector2.Lerp (transform.position, camTarget.position, Time.deltaTime * trackingSpeed);
+
+			Vector3 camPosition = new Vector3 (newPos.x, newPos.y, -10f);
+			Vector3 v3 = camPosition;
+			float clampX = Mathf.Clamp (v3.x, minX, maxX);
+			float clampY = Mathf.Clamp (v3.y, minY, maxY);
+			transform.position = new Vector3 (clampX, clampY, -10f);
+		}
+	}
+}
---
+++ Assets/Scripts/SoyBoyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D), typeof(Animator))]
+public class SoyBoyController : MonoBehaviour {
+	
+	public float				speed = 14f;
+	public float				accel = 6f;
+	private Vector2				input;
+	private SpriteRenderer		sr;
+	private Rigidbody2D			rb;
+	private Animator			animator;
+
+	void Awake () {
+		sr = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input.x = Input.GetAxis ("Horizontal");
+		input.y = Input.GetAxis ("Jump");
+
+		if (input.x > 0f)
+			sr.flipX = false;
+		else if (input.x < 0f)
+			sr.flipX = true;
+	}
+
+	void FixedUpdate () {
+		float acceleration = accel;
+		float xVelocity = 0f;
+
+		if (input.x == 0)
+			xVelocity = 0f;
+		else
+			xVelocity = rb.velocity.x;
+
+		rb.AddForce (new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
+
+		rb.velocity = new Vector2 (xVelocity, rb.velocity.y);
+	}
+}
